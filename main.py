1ebee08209c4e3986ed7b0d901349e9ceb72462c
EpsilonCrab <vetervonyuchy@yandex.ru>
2023-10-25 23:33:58 +0300

    light_theme/bug_fix

--- main.py
+++ main.py
@@ -43,6 +43,12 @@ GR_PROJ_label_list = [' ', 'g1', 'codkon', 'codvuz', 'z2', 'g7', 'g5', 'g2', 'g2
 VUZ_label_list = [' ', 'codvuz', 'z1', 'z1full','z2','region', 'city', 'status', 'obl','oblname','gr_ved','prof']
 window_specs = [['GR_KONK',0], ['GR_PROJ',1], ['VUZ',2]]
 
+GR_KONK_label_list_ru = [' ', 'Конкурс', 'Код конкурса', 'Плановый объем гранта', 'Фактический объем гранта', '1-ый кв', '2-ой кв', '3-ий кв', '4-ый кв', 'кол-во НИР по грантам']
+GR_PROJ_label_list_ru  = [' ', 'Код НИР', 'Код конкурса', 'Код вуза', 'ВУЗ', 'ГРНТИ', 'Плановый объем гранта', 'Фактический объем гранта',
+ '1-ый кв', '2-ой кв', '3-ий кв', '4-ый кв', 'НИР', 'Руководитель НИР', 'Должность', 'Ученое  звание', 'Ученая степень']
+VUZ_label_list_ru  = [' ', 'Код вуза', 'ВУЗ', 'Юр наз-ие ВУЗа','Наименование','Фед. округ', 'Город', 'Статус', 'Субъект','Область','gr_ved','Направленность']
+
+
 table_name_id = []
 
 high_list = []
@@ -233,7 +239,7 @@ def highlight_rows(sender, specs, user_data):
         cols.append(dpg.get_value(_row+a))
     if(specs):
         if(len(high_list)==0):
-            dpg.highlight_table_row(table = _table, row = _row_, color = (0,0,100))
+            dpg.highlight_table_row(table = _table, row = _row_, color = (233,118,246))
             high_list.append([sender,_row_,_table])
             high_list.append(cols)
             print(high_list)
@@ -241,7 +247,7 @@ def highlight_rows(sender, specs, user_data):
             dpg.unhighlight_table_row(table = high_list[0][2], row = high_list[0][1])
             dpg.set_value(high_list[0][0],False)
             high_list.clear()
-            dpg.highlight_table_row(table = _table, row = _row_, color = (0,0,100))
+            dpg.highlight_table_row(table = _table, row = _row_, color = (233,118,246))
             high_list.append([sender,_row_,_table])
             high_list.append(cols)
             print(high_list)
@@ -249,8 +255,8 @@ def highlight_rows(sender, specs, user_data):
         dpg.unhighlight_table_row(table = _table, row = _row_)
         high_list.clear()
         print(high_list)
-
-    edit_btn()
+    if len(high_list)!=0:
+        edit_btn()
 
 def sort_algo(col_id, s_dir, row, sender, ran):
     if(dpg.get_item_label(sender) == 'gr_konk'):
@@ -302,6 +308,7 @@ def sort_algo(col_id, s_dir, row, sender, ran):
                     dpg.add_selectable(height = 27, width = 27, callback = highlight_rows, user_data = info[1]+2)
                 else:
                     dpg.add_text(f"{_row[j]}")
+    high_list.clear()
 
 def sort_callback(sender, sort_specs):
     print(sort_specs)
@@ -399,7 +406,7 @@ def reset_filter(sender, specs,user_data):
                         filter_mas_1.append([str(x) for x in row])
                     for c in range(len(filter_mas_1)):
                         filter_mas_1[c] = filter_mas_1[c][0]
-                    combo = dpg.add_combo(parent = 'filter_combo_input', label = eval(i[0].upper() + '_label_list[j+1]') , callback = filtering,
+                    combo = dpg.add_combo(parent = 'filter_combo_input', label = eval(i[0].upper() + '_label_list_ru[j+1]') , callback = filtering,
                                                                 user_data = [user_data,eval("filter_string_" + i[0].upper())], items = filter_mas_1)
                     combo_id_mas.append(combo)
                     filter_mas_1.clear()
@@ -526,7 +533,7 @@ def filtering(sender,specs, user_data):
                         a+=1
                 if a == len(specs_filter_list):
                     dpg.delete_item(combo_id_mas[j])
-                    combo_id_mas[j] = dpg.add_combo(parent = 'filter_combo_input', label = eval(i[0].upper() + '_label_list[j+1]') , callback = filtering,
+                    combo_id_mas[j] = dpg.add_combo(parent = 'filter_combo_input', label = eval(i[0].upper() + '_label_list_ru[j+1]') , callback = filtering,
                                                                 user_data = [user_data[0],eval("filter_string_" + i[0].upper())], items = filter_mas_1)
                 else:
                     dpg.disable_item(combo_id_mas[j])
@@ -560,7 +567,6 @@ def filtering(sender,specs, user_data):
                             else:
                                 dpg.add_text(f"{_row[j]}")
 
-
 def filter_btn(sender):
     if(not dpg.does_item_exist('FILTER')):
         with dpg.window(tag = 'FILTER', label = "Фильтр", width = 300, height = 300, on_close = closing_filter_window):
@@ -569,6 +575,8 @@ def filter_btn(sender):
     dpg.show_item('FILTER')
 
 def delete_btn(sender):
+    if len(high_list)==0 or dpg.get_item_label(high_list[0][2])!='gr_proj':
+        return
     if(not dpg.does_item_exist('DELETE')):
         with dpg.window(tag = 'DELETE', label = "Удаление", width = 300, height = 200, modal = True, pos = (35,50)):
             if len(high_list)!=0:
@@ -592,29 +600,35 @@ def delete_btn(sender):
 
 def edit_btn():
     if(not dpg.does_item_exist('EDIT')):
-        if(not len(high_list)):
-            with dpg.window(tag = 'EDIT', label = "Редактирование", width = 300, height = 400, pos = (30,250)):
-                dpg.add_text('Выберите строку для редактирования', tag = 'choose_row')
+        if len(high_list)==0 or dpg.get_item_label(high_list[0][2])!='gr_proj':
+            return
         else:
-            with dpg.window(tag = 'EDIT', label = "Редактирование", width = 300, height = 400, pos = (30,250)):
-                dpg.delete_item('choose_row')
-                dpg.add_text('Редактировать строку')
-                for i in range(16):
-                    dpg.add_input_text(label = GR_PROJ_label_list[i+1], default_value = high_list[1][i], on_enter = True, callback = edit_callback)
-                    edit_input.append(dpg.last_item())
+            if(not len(high_list)):
+                with dpg.window(tag = 'EDIT', label = "Редактирование", width = 300, height = 400, pos = (30,250)):
+                    dpg.add_text('Выберите строку для редактирования', tag = 'choose_row')
+            else:
+                with dpg.window(tag = 'EDIT', label = "Редактирование", width = 300, height = 400, pos = (30,250)):
+                    dpg.delete_item('choose_row')
+                    dpg.add_text('Редактировать строку')
+                    for i in range(16):
+                        dpg.add_input_text(label = GR_PROJ_label_list[i+1], default_value = high_list[1][i], on_enter = True, callback = edit_callback)
+                        edit_input.append(dpg.last_item())
     else:
-        if(dpg.does_item_exist('choose_row')):
-            dpg.delete_item('choose_row')
-        for x in edit_input:
-            dpg.delete_item(x)
-        edit_input.clear()
-        if(len(high_list)):
-            for i in range(16):
-                dpg.add_input_text(parent = 'EDIT', label = GR_PROJ_label_list[i+1], default_value = high_list[1][i], on_enter = True, callback = edit_callback)
-                edit_input.append(dpg.last_item())
+        if len(high_list)==0 or dpg.get_item_label(high_list[0][2])!='gr_proj':
+            return
         else:
-            dpg.add_text('Выберите строку для редактирования', tag = 'choose_row', parent = 'EDIT')
-    dpg.show_item('EDIT')
+            if(dpg.does_item_exist('choose_row')):
+                dpg.delete_item('choose_row')
+            for x in edit_input:
+                dpg.delete_item(x)
+            edit_input.clear()
+            if(len(high_list)):
+                for i in range(16):
+                    dpg.add_input_text(parent = 'EDIT', label = GR_PROJ_label_list[i+1], default_value = high_list[1][i], on_enter = True, callback = edit_callback)
+                    edit_input.append(dpg.last_item())
+            else:
+                dpg.add_text('Выберите строку для редактирования', tag = 'choose_row', parent = 'EDIT')
+        dpg.show_item('EDIT')
 
 def add_row_btn(sender):
     if(not dpg.does_item_exist('ADD_ROW')):
@@ -636,14 +650,15 @@ def add_copy_btn(sender):
 """
 
 def inst_window(sender):
-    with dpg.window(tag = 'INS', label = "Инструменты", width = 200, height = 200, on_close = closing_inst_window):
+    with dpg.window(tag = 'INS', label = "Инструменты", width = 250, height = 250, on_close = closing_inst_window):
         dpg.set_item_pos("INS",(30,45))
+        dpg.add_text(default_value = 'Для всех таблиц')
         dpg.add_button(label = 'Фильтр', callback = filter_btn)
+        dpg.add_text(default_value = 'Для таблицы проектов')
         dpg.add_button(label = 'Удалить', callback =  delete_btn)
         dpg.add_button(label = 'Редактировать', callback = edit_btn)
         #dpg.add_button(label = 'Копировать', callback = add_copy_btn)
         dpg.add_button(label = 'Добавить в конец', callback = add_row_btn)
-        
 
 with dpg.window(tag = 'Main Window', height = 720, width = 1280):
     dpg.set_primary_window("Main Window", True)
@@ -661,13 +676,35 @@ with dpg.window(tag = 'Main Window', height = 720, width = 1280):
                 dpg.set_item_pos(i[0],(400,50))
                 with dpg.table(label = i[0].lower(),header_row=True, row_background=True,
                             borders_innerH=True, borders_outerH=True, borders_innerV=True,
-                            borders_outerV=True, resizable = False, width = 800,height = 600, scrollX=True, scrollY=True, no_keep_columns_visible=True, 
+                            borders_outerV=True, resizable = False, scrollX=True, scrollY=True, no_keep_columns_visible=True, 
                             policy = dpg.mvTable_SizingFixedFit, sortable = True, callback=sort_callback, sort_multi=True):
                     table_name_id.append([dpg.last_item(), i[0].lower()])
                     for _label in eval( i[0] + '_label_list'):
                         dpg.add_table_column(label=_label)
 
                     table_creation(table_list_name_size[i[1]])
+
+
+with dpg.theme() as global_theme:
+    with dpg.theme_component(dpg.mvAll):
+        dpg.add_theme_color(dpg.mvThemeCol_WindowBg, (235, 235, 235))
+        dpg.add_theme_color(dpg.mvThemeCol_Text, (0, 0, 0))
+        dpg.add_theme_color(dpg.mvThemeCol_MenuBarBg, (235, 235, 235))
+        dpg.add_theme_color(dpg.mvThemeCol_Button, (128, 128, 128))
+        dpg.add_theme_color(dpg.mvThemeCol_TableRowBg, (128, 128, 128))
+        dpg.add_theme_color(dpg.mvThemeCol_TableRowBgAlt, (235, 235, 235))
+        dpg.add_theme_color(dpg.mvThemeCol_TableHeaderBg, (235, 235, 235))
+        dpg.add_theme_color(dpg.mvThemeCol_PopupBg, (235, 235, 235))
+        dpg.add_theme_color(dpg.mvThemeCol_ScrollbarGrab, (235, 235, 235))
+        dpg.add_theme_color(dpg.mvThemeCol_Header, (128, 128, 128))
+        dpg.add_theme_color(dpg.mvThemeCol_Tab, (128, 128, 128))
+        dpg.add_theme_color(dpg.mvThemeCol_HeaderActive, (128, 128, 128))
+        dpg.add_theme_color(dpg.mvThemeCol_TitleBg, (128, 128, 128))
+        dpg.add_theme_color(dpg.mvThemeCol_ScrollbarBg , (60, 60, 60))
+        dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (128, 128, 128))
+        dpg.add_theme_color(dpg.mvThemeCol_TitleBgActive, (233,118,246))
+
+dpg.bind_theme(global_theme)
 
 dpg.create_viewport(title='Support of competitions for grants')
 dpg.setup_dearpygui()
